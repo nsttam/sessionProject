baa7c7bf3595ca7e399a72ec82301d83a4fcb1b9
Tamara <tamararavagnan@gmail.com>
2019-04-17 14:58:12 +0200

    sessionSet

--- class/session/session.php
+++ class/session/session.php
@@ -1,5 +1,44 @@
 <?php
 class Session{
+    public $attributs=[]; // tableau ou on aura tous les attributs
+
+    /**
+     * Session constructor.
+     * @param array $attributs
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributs()
+    {
+        return $this->attributs;
+    }
+
+
+
+    public function add($key, $value){
+        $this->attributs[$key]=$value; //on ajoute attribut à tableau attributs
+
+    }
+
+    public function update($key, $value){
+        $this->attributs[$key]=$value;
+
+    }
+
+    public function __set($key, $value){  //
+        $this->add ($key, $value);
+    }
+
+    public function __get($key){
+        return $this->attributs[$key];
+    }
+
 
 
 
--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 $page = __FILE__;
 include 'include.php';
 
+$session1 = new Session();
+
+$session1->key1 = "toto"; //set on le donne des valeurs //je suis n train d'appeler la fx __set qui appelle la fx add
+
+echo $session1->key1; //get
+
+var_dump($session1);
 
 ?>
